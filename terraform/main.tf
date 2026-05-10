4b1297eafd4831930c311115ff00da7b6d52ff77
aviteshs <mail4avitesh@gmail.com>
2026-05-10 19:42:42 +0530

    updated2

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,6 +106,19 @@ resource "aws_instance" "web_server" {
 # -----------------------------
 # Output
 # -----------------------------
-output "public_ip" {
+output "ec2_public_ip" {
   value = aws_instance.web_server.public_ip
 }
+
+output "grafana_url" {
+  value = "http://${aws_instance.web_server.public_ip}:3000"
+}
+
+output "prometheus_url" {
+  value = "http://${aws_instance.web_server.public_ip}:9090"
+}
+
+output "docker_compose_check" {
+  value = "docker ps"
+}
+
